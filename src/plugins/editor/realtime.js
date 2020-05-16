9be706f3863bf842574a5eaa1efe4cecaf510f9c
samiell <danquah.samiell@gmail.com>
2020-05-16 16:47:50 +0000

    refactor isClient

--- src/plugins/editor/realtime.js
+++ src/plugins/editor/realtime.js
@@ -8,32 +8,28 @@ import { fromBase64, toBase64 } from '@aws-sdk/util-base64-browser'
 
 const ydoc = new Y.Doc()
 const initYDoc = (page) => {
-    if (process.isClient) {
-        if (
-            typeof page.content === 'string' &&
-            page.content.length !== 0
-        ) {
-            this.page.content = fromBase64(this.page.content)
-            Y.applyUpdate(ydoc, page.content)
-        }
-        ydoc.on('update', update => {
-            page.content = toBase64(Y.encodeStateAsUpdate(ydoc))
-            updatePage(page)
-        })
-        return ydoc.getXmlFragment('prosemirror')
+    if (
+        typeof page.content === 'string' &&
+        page.content.length !== 0
+    ) {
+        this.page.content = fromBase64(this.page.content)
+        Y.applyUpdate(ydoc, page.content)
     }
+    ydoc.on('update', update => {
+        page.content = toBase64(Y.encodeStateAsUpdate(ydoc))
+        updatePage(page)
+    })
+    return ydoc.getXmlFragment('prosemirror')
 }
 
 const initProvider = (page) => {
-    if (process.isClient) {
-        const provider = new WebrtcProvider(
-            page.room, ydoc, {
-                password: page.secret,
-                maxConns: 70 + Math.floor(Math.random() * 70),
-            }
-        )
-        return provider.awareness
-    }
+    const provider = new WebrtcProvider(
+        page.room, ydoc, {
+            password: page.secret,
+            maxConns: 70 + Math.floor(Math.random() * 70),
+        }
+    )
+    return provider.awareness
 }
 
 export default class Realtime extends Extension {
